b7d7b05f4c73aa398a4c67c82d99733587f9f09b
arrghh1 <rowancguard-misc@yahoo.com.au>
2020-04-24 18:07:55 +1000

    basics of spider getting started

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Australian Financial Complaints Authority  Power BI Scraper.
 
 Using Python, Scrapy, and Selenium. The intent was originally to take the XHR message from powerBI for the table views specifically, and use that, but there is some kind of dark magic making this a particularly difficult problem (happy to chat to anyone about this!) Instead, I'll use selenium, and pull out the div's for the table, and hope that order is somehow achievable from this.
+
+Note: Key components missing: `chromedriver.exe` no point backing that to github, so if you need to run this, you'll need to install that.
+Also:
+    `pip install selenium`
+    `pip install scrapy-selenium`
--- afca/spiders/products.py
+++ afca/spiders/products.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from  scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from shutil import which
 
 
 class ProductsSpider(scrapy.Spider):
     name = 'products'
-    allowed_domains = ['https://data.afca.org.au/banking-and-finance/']
-    start_urls = ['http://https://data.afca.org.au/banking-and-finance//']
+    allowed_domains = ['data.afca.org.au']
+    start_urls = ['http://https://data.afca.org.au/banking-and-finance']
 
+    def __init__(self):
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        chrome_path = which("chromedriver")
+        driver = webdriver.chrome(executable_path=chrome_path, options = chrome_options)
+        driver.set_window_size(1920,1080)
+        driver.get(start_urls)
+
+        ##need to work out where to click to get started on the looping!
+    
+    
     def parse(self, response):
-        pass
+        resp = Selector(text=self.html)
+        for MyElements in resp.xpath("//")
+            yield{
+                #things
+            }
